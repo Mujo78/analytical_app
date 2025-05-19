cea26b8b9679ef2421bb3a7a740565bb85120f4b
Mujo78 <mujo.kalabuzic.20@size.ba>
2025-05-19 20:20:51 +0200

    initial commit server

---
+++ server/controllers/BasicController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicController : ControllerBase
+    {
+    }
+}
